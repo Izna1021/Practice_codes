7e63219c23392fc53133e13008387085114e705c
Izna <izna8402@gmail.com>
2023-04-15 22:25:38 +0530

    java codes

---
+++ Example4.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+import java.io.File;
+public class Example4{
+   public static void main(String[] args) {
+      Scanner in = new Scanner(new File("test.in"));
+      // do something ...
+   }
+}
---
+++ Example8.java
@@ -0,0 +1,14 @@
+public class Example8 {
+   public static void main(String[] args) {
+      char operator = '%';                  // assumed either '+', '-', '*', '/' only
+      int operand1 = 5, operand2 = 6, result = 0;
+      switch (operator) {
+         case '+': result = operand1 + operand2; break;
+         case '-': result = operand1 - operand2; break;
+         case '*': result = operand1 * operand2; break;
+         case '/': result = operand1 / operand2; break;
+         default: assert false : "Unknown operator: " + operator;  // not plausible here
+      }
+      System.out.println(operand1 + " " + operator + " " + operand2 + " = " + result);
+   }
+}

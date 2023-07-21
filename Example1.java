e1c37e48ac6fd43c439e6b4817830bbb6bb4ce1f
Bibi Izna <izna8402@gmail.com>
2023-07-21 22:59:57 +0530

    Update Example1.java

--- Example1.java
+++ Example1.java
@@ -11,10 +11,6 @@ class Example1 {
     System.out.println("You should not divide a number by zero");
     }
     catch (Exception e) {
-    /* This is a generic Exception handler which means it can handle
-    * all the exceptions. This will execute if the exception is not
-    * handled by previous catch blocks.
-    */
     System.out.println("Exception occurred");
     }
     System.out.println("I'm out of try-catch block in Java.");

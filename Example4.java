5df083a005ad62f76c4518ebae8fbf9c9204de42
Bibi Izna <izna8402@gmail.com>
2023-07-30 17:32:57 +0530

    Update Example4.java

--- Example4.java
+++ Example4.java
@@ -3,6 +3,6 @@ import java.io.File;
 public class Example4{
    public static void main(String[] args) {
       Scanner in = new Scanner(new File("test.in"));
-      // do something ...
+      
    }
 }

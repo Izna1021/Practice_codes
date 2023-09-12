567f0645cfb0138595977952415ce35d07e4e62f
Bibi Izna <izna8402@gmail.com>
2023-09-12 23:55:49 +0530

    Update Code.java

--- Code.java
+++ Code.java
@@ -2,7 +2,7 @@ class Solution {
     public int countOrders(int n) {
         long mod = 1_000_000_007;
         long result = 1;
-        //
+
         for (int i = 1; i <= n; i++) {
             int order = 2 * i - 1;
             result = (result * order * (order + 1) / 2) % mod;
